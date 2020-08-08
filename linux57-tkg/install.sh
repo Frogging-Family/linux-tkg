f8a182fb07a26ab934340408069c74e4a9839640
Adel KARA SLIMANE <adel@karaslimane.com>
2020-08-08 15:48:37 +0200

    linux57: install.sh: remove unneeded variable

--- linux57-tkg/install.sh
+++ linux57-tkg/install.sh
@@ -19,8 +19,6 @@ plain() {
 # alias plain=echo
 set -e
 
-# Variable to know if the user command has been recognised
-_command_recognised=0
 _where=`pwd`
 srcdir="$_where"
 
@@ -63,8 +61,6 @@ if [ "$1" == "install" ] || [ "$1" == "config" ]; then
   if [ "$1" == "config" ]; then
     _distro=""
   fi
-
-  _command_recognised=1
 
   if [ -d linux-${_basekernel}.orig ]; then
     rm -rf linux-${_basekernel}.orig
@@ -118,8 +114,6 @@ fi
 
 if [ "$1" == "install" ]; then
 
-  _command_recognised=1
-
   # Use custom compiler paths if defined
   if [ -n "${CUSTOM_GCC_PATH}" ]; then
     PATH=${CUSTOM_GCC_PATH}/bin:${CUSTOM_GCC_PATH}/lib:${CUSTOM_GCC_PATH}/include:${PATH}
@@ -171,7 +165,6 @@ if [ "$1" == "install" ]; then
 fi
 
 if [ "$1" == "uninstall" ]; then
-  _command_recognised=1
 
   cd "$_where"
 
