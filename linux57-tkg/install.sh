fe4c9068895f07f2974251f3af73377b18d4c971
Adel KARA SLIMANE <adel@karaslimane.com>
2020-08-31 01:31:25 +0200

    linux57: add ubuntu/debian dependency install

--- linux57-tkg/install.sh
+++ linux57-tkg/install.sh
@@ -80,7 +80,7 @@ if [ "$1" = "install" ] || [ "$1" = "config" ]; then
 
   if [ "$_distro" = "Ubuntu" ] || [ "$_distro" = "Debian" ]; then
     msg2 "Installing dependencies"
-    sudo apt install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bison flex -y
+    sudo apt install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bison flex qtbase5-dev -y
   elif [ "$_distro" = "Fedora" ]; then
     msg2 "Installing dependencies"
     sudo dnf install fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby qt5-devel libXi-devel gcc-c++ git ccache flex bison elfutils-libelf-devel openssl-devel dwarves rpm-build -y
