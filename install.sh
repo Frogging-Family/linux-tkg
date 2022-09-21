4a9f4d3145c1ff63b09072663829c166bb524571
Amar Al-Zubaidi <92645199+amarakon@users.noreply.github.com>
2022-09-21 12:26:23 -0500

    Support `doas` and `su` for root authentication (#593)

--- install.sh
+++ install.sh
@@ -17,6 +17,14 @@ set -e
 _where=`pwd`
 srcdir="$_where"
 
+# Command used for superuser privileges (`sudo`, `doas`, `su`)
+if [ ! -x "$(command -v sudo)" ]; then
+	if [ -x "$(command -v doas)" ]; then sudo() { doas "$@"; }
+	elif [ -x "$(command -v su)" -a -x "$(command -v xargs)" ]; then
+		sudo() { echo "$@" | xargs -I {} su -c '{}'; }
+	fi
+fi
+
 source customization.cfg
 
 source linux-tkg-config/prepare
